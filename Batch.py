7fa9d30b1e27eb310e8c3bf5bdb32a47388e8bd6
Nuked22 <56643647+Nuked22@users.noreply.github.com>
2022-04-15 02:19:41 -0400

    init

--- Batch.py
+++ Batch.py
@@ -1,4 +1,6 @@
 
+from Member import * 
+
 class Batch():
     def __init__(self, name, instructor, sizeCap):
         self.name = name
@@ -6,3 +8,20 @@ class Batch():
         self.sizeCap = sizeCap
         self.currentSize = 0
         self.students = []
+        self.membersA = []
+        self.membersP = []
+        self.functions = ["Enroll","Sort by attendence","Sort by fee paid"]
+
+
+    def get_batch(self):
+        return self.students #returns a list of members currently enrolled in the batch
+    
+    @staticmethod
+    def enroll(): #enrolls member to the Batch
+        pass
+
+    def sort_batch_attendence(self): #sorts batch by the the number of attendence
+        pass
+
+    def sort_batch_paid(self): #sorts batch by the amount of times they paid or not
+        pass
