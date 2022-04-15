35c3bb04b39f1c0340d030173a87588128e0f8ca
kheir49 <73240615+kheir49@users.noreply.github.com>
2022-04-15 16:34:03 -0400

    Update Treasurer.py
    Added sorting and income logging functionality to Treasurer,py

--- Treasurer.py
+++ Treasurer.py
@@ -4,26 +4,153 @@ class Treasurer(User):
     def __init__(self, name, password, contactInfo, authority):
         #Inherit
         super().__init__(name, password, contactInfo, authority)
+
+        self.memberList = []
+
+        #Log of those who have paid in advance
+        self.payList = []
+
+        #Total Club balance
+        self.clubBalance = 0
+
+        #Various expenses are logged here
+        self.hallCost = 0
+        self.coachCost = 0
+        self.otherCost = 0
+        self.expenses = 0
+
+        #Dictionary containing all members who have paid and the amount paid
+        #Income and revenue is also logged in these variables
+        self.memberPay = {}
+        self.otherIncome = 0
+        self.revenue = 0
+
+        #Total profits for this month, and all previous month profits logged here
+        self.profits = 0
+        self.profitList = []
+
+        #Total amount of debt
+        self.coachDebt = 0
+        self.hallDebt = 0
+
         #Useable Functions
         self.functions = ["Signout", "Balance", "Hire", "Pay", "Members"]
-    def currentProfit():
-    def Debt():
-    def SortMembers(memberList, sortType): 
-    def applyDiscount(allMembers, discount):
+
+    #Adds a member to the list of advance payment
+    def addPaylist(self, payee, amount):
+        self.payList.append(payee)
+        self.memberPay[payee] = amount
+        self.revenue += amount
+
+    #Adds total revenue, second function adds other income
+    def addRevenue(self, payee, amount):
+        self.memberPay[payee] = amount
+        self.revenue += amount
+    def addOtherIncome(self):
+        self.revenue += self.otherIncome
+
+    #Returns current profit value
+    def currentProfit(self):
+        return self.revenue
+
+    #Set various costs
+    def setHallCost(self, amount):
+        self.hallCost = amount
+    def setCoachCost(self, amount):
+        self.coachCost = amount
+    def setOtherCost(self, amount):
+        self.otherCost = amount
+
+    #Set expenses for this month and retrieve them
+    def setExpenses(self):
+        self.expenses = self.coachCost + self.hallCost + self.otherCost
+    def getExpenses(self):
+        return self.expenses
+
+    #Set profits for this month
+    def setProfits(self):
+        self.profits = self.revenue - self.expenses
+
+    #Below function creates the income statement
+    def incomeStatement(self):
+        print("Revenue:")
+        for key in self.memberPay:
+            print("Member", key +":\t", self.memberPay.get(key))
+        print("Other Income:", self.otherIncome)
+        print("Total Revenues:", self.revenue)
+        print("\nExpenses:\nHall Costs:\t", self.hallCost,
+              "\nCoach Cost:\t", self.coachCost)
+        print("Total Expenses:", self.getExpenses())
+
+        print("This month's profit:", self.profits)
+        self.profitList.append(self.profits)
+
+        print("Monthly Profits:")
+        for x in range(len(self.profitList) - 1):
+            print("Month", str(x+1) + ":", self.profitList[x])
+            if x == len(self.profitList) - 2:
+                print("Month", str(x+2) + "(Current Month):", self.profitList[x+1])
+
+    #Two functions below are used to sort the member list
+    #takePay sorts by the most payments
+    def takePay(elem):
+        return elem[1]
+    #take Attendance sorts by the most attendances
+    def takeAttendance(elem):
+        return elem[2]
+
+    def SortMembers(self, sortType):
+        sortList = self.memberList
+        if sortType == "Paid":
+            sortList.sort(key=self.takePay, reverse=True)
+        elif sortType == "Attendance":
+            sortList.sort(key=self.takeAttendance, reverse=True)
+        return sortList
+
+
+    def applyDiscount(self, member):
+        discount = 0.0
+        memTup = None
+        for x in self.memberList:
+            if member == x[0]:
+                memTup = x
+
+        sortList = self.SortMembers(self.memberList, "Paid")
+        payFilter = filter(lambda x: x[1] == 3, sortList)
+        payList = list(payFilter)
+        for x in payList:
+            if member == x[0]:
+                discount += 0.10
+                self.memberList.remove(x)
+                memTup = (member, 0, memTup[2])
+                self.MemberList.append(memTup)
+
+        sortList = self.SortMembers(self.memberList, "Attendance")
+        sortList = sortList[:10]
+        for x in sortList:
+            if member == x[0]:
+                discount += 0.10
+                self.memberList.remove(x)
+                memTup = (member, memTup[1], 0)
+                self.memberList.append(memTup)
+        return discount
+
+
     def getFunction(self):
         return self.functions
+
     def useFunction(self, userInput):
         #Balance
-        if(userInput.lower() == functions[1].lower()):
+        if(userInput.lower() == self.functions[1].lower()):
             print(self.balance)
         #Hire
-        elif(userInput.lower() == functions[2].lower()):
+        elif(userInput.lower() == self.functions[2].lower()):
             self.Hire()
         #Pay
-        elif(userInput.lower() == functions[3].lower()):
+        elif(userInput.lower() == self.functions[3].lower()):
             self.Pay()
         #List Members
-        elif(userInput.lower() == functions[4].lower()):
+        elif(userInput.lower() == self.functions[4].lower()):
             self.Members()
         #Invalid
         else:
