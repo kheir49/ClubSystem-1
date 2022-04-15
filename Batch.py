45084453d693fd70749b39a0fcee4ac33e5be15c
Nuked22 <56643647+Nuked22@users.noreply.github.com>
2022-04-15 15:48:10 -0400

    Update Batch.py

--- Batch.py
+++ Batch.py
@@ -12,7 +12,7 @@ class Batch():
         self.membersA = []
         self.membersP = []
         self.announcements = []
-        self.functions = ["Enroll","Sort by attendence","Sort by fee paid"]
+        self.functions = ["Enroll"]
 
 
     def get_batch(self):
@@ -21,17 +21,17 @@ class Batch():
     def enroll(self, Member): #enrolls member to the Batch, adds member object to the students list
         self.students.append(Member)
 
-    def sort_batch_attendence(self): #sorts batch by the the number of attendence
-        dicts = {}
+    #def sort_batch_attendence(self): #sorts batch by the the number of attendence
+        #dicts = {}
         
-        for i in range(len(self.students)):
-            dicts[self.students[i].name] = self.students[i].attended
+        #for i in range(len(self.students)):
+            #dicts[self.students[i].name] = self.students[i].attended
 
-        sorted(dicts.items(), key=lambda x: x[1], reverse=True)
+        #sorted(dicts.items(), key=lambda x: x[1], reverse=True)
 
-        self.memberA = list(dicts.keys())  
+        #self.memberA = list(dicts.keys())  
 
-        return self.memberA      
+        #return self.memberA      
 
-    def sort_batch_paid(self): #sorts batch by the amount of times they paid or not
-        pass
+    #def sort_batch_paid(self): #sorts batch by the amount of times they paid or not
+        #pass
