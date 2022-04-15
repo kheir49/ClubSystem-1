d455799b21cd61dee3aa856ced8b99bf674e9a35
Nuked22 <56643647+Nuked22@users.noreply.github.com>
2022-04-15 15:40:06 -0400

    Update Batch.py

--- Batch.py
+++ Batch.py
@@ -18,11 +18,20 @@ class Batch():
     def get_batch(self):
         return self.students #returns a list of members currently enrolled in the batch
     
-    def enroll(): #enrolls member to the Batch
-        pass
+    def enroll(self, Member): #enrolls member to the Batch, adds member object to the students list
+        self.students.append(Member)
 
     def sort_batch_attendence(self): #sorts batch by the the number of attendence
-        pass
+        dicts = {}
+        
+        for i in range(len(self.students)):
+            dicts[self.students[i].name] = self.students[i].attended
+
+        sorted(dicts.items(), key=lambda x: x[1], reverse=True)
+
+        self.memberA = list(dicts.keys())  
+
+        return self.memberA      
 
     def sort_batch_paid(self): #sorts batch by the amount of times they paid or not
         pass
